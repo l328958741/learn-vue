fc5f1ecc19ff54e4ed697bcc7e5333bde87ddc68
l328958741 <328958741@qq.com>
2018-05-21 21:05:14 +0800

    update

--- webpack_template_vue/server/server.js
+++ webpack_template_vue/server/server.js
@@ -13,14 +13,7 @@ server.use(async (ctx, next) => {
 let r = router();
 
 r.get('/list', ctx => {
-    ctx.body = [
-        {id:1, name: '111', price:345, sales: 124},
-        {id:2, name: '222', price:22, sales: 2345},
-        {id:3, name: '333', price:56, sales: 5567},
-        {id:4, name: '444', price:44, sales: 5553},
-        {id:5, name: '5555', price:1235, sales: 234},
-        {id:6, name: 'iphone X', price:9888, sales: 234},
-    ];
+    ctx.body = [12, 4, 5, 6];
 });
 
 server.use(r.routes());
